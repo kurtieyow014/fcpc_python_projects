c422039c4ffdc8a9d8c68a613b6324b3e2ef073e
kurtieyow014 <45374632+kurtieyow014@users.noreply.github.com>
2018-11-27 07:48:52 +0800

    Create dynamic_aguilar.py

---
+++ dynamic_aguilar.py
@@ -0,0 +1,12 @@
+#accepts input from user
+name = input ("Enter your name")
+print ("Your name is: " + name)
+
+#accepts numerical input
+fnum = int (input("Enter first number:"))
+snum = int (input("Enter second number:"))
+
+to_int_value = int("3")
+
+print(fnum + snum)
+print(to_int_value ** 3)
