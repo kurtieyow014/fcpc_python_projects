c3fed6dbf6e526e731d92f1bb7161f8de044ef47
kurtieyow014 <45374632+kurtieyow014@users.noreply.github.com>
2018-11-27 07:47:13 +0800

    Create sample.aguilar.py

---
+++ sample.aguilar.py
@@ -0,0 +1,39 @@
+#Sample Test
+
+#Strings
+balbin = "classmate"
+ralph = "classmate"
+mark = "classmate"
+
+#Numeral values
+balbin = 18
+ralph = 18
+mark = 18
+
+print (balbin)
+print (ralph)
+print (mark)
+
+print (18)
+print (18)
+print (18)
+
+#Arithmetic
+addition = 1+1
+subtraction = 1-1
+multiplication =  1*1
+division = 1/1
+modulus = 5&3
+
+print (addition)
+print (subtraction)
+print (multiplication)
+print (division)
+print (modulus)
+
+if addition > division:
+    print ("Hello")
+elif modulus > multiplication:
+    print ("Modulus")
+else:
+    print ("Hi")
