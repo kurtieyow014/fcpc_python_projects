dc54080f413e1aed1fb9fe370a46a747052851e4
kurtieyow014 <45374632+kurtieyow014@users.noreply.github.com>
2018-11-27 07:47:50 +0800

    Create  loop_aguilar.py

---
+++ loop_aguilar.py
@@ -0,0 +1,23 @@
+for x in range (2,14,2):
+    print (x)
+
+for x in range (0,14,3):
+    print (x)
+
+for x in range (4,16,4):
+    print (x)
+
+for x in range (3,13,3):
+    print (x)
+
+#nested loop sample
+
+for y in range (1,11):
+    for z in range (1,11):
+        print (z * y,end='\t')
+    print()
+
+for y in range (2,21):
+    for z in range(1,11):
+        print(z * y, end='\t')
+    print()
